8cb9663b91c8078d130ddd80bace06164b830ce7
George Plukov <gplukov@gmail.com>
2014-11-01 01:38:01 -0400

    c

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import yopy
+from yopy import Yo
+import config
+import requests
+from pymongo import MongoClient
 
+def main ():
+	yo = Yo(config.devKey)
+	print(yo.youser("GEORGEPLUKOV", ""))
 
-
-
-
-
-
+main()
