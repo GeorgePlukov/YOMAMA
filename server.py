d39300831ef24dfad174dd7d10b90f997345a109
George Plukov <gplukov@gmail.com>
2014-11-01 16:07:26 -0400

    Added jokes and server

--- server.py
+++ server.py
@@ -3,6 +3,8 @@
 from flask import request, Flask
 import requests
 import config
+import Jokes
+import random
 YO_API_TOKEN = config.devKey
 
 app = Flask(__name__)
@@ -10,14 +12,21 @@ app = Flask(__name__)
 
 @app.route("/yo/")
 def yo():
+	# get the jokes list from the Jokes file
+    jokes = Jokes.jokeList
+    # Generate a random number 
+    ran = random.randint(0, len(jokes)-1)
+
 
     # extract and parse query parameters
     username = request.args.get('username')
 
- #  print "We got a Yo from " + username
+
+     #print "We got a Yo from " + username
     joke = username + " joke"
+
     # Yo the result back to the user
-    requests.post("http://api.justyo.co/yo/", data={'api_token': YO_API_TOKEN, 'username': username, 'link': 'http://54.86.232.229/YOMAMA/'})
+    requests.post("http://api.justyo.co/yo/", data={'api_token': YO_API_TOKEN, 'username': username, 'link': 'http://www.yotext.co/show/?text=' + jokes[ran]})
 #'http://www.yotext.co/show/?text=' + joke}
     # OK!
     return 'OK'
